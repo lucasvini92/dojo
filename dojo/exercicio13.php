d972af6506236120a0d2d5f825a7d3625a71163d
lucas <lucasvsteofilo@gmail.com>
2021-05-30 22:09:49 -0300

    exercicio13

---
+++ dojo/exercicio13.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Semana 01</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+
+    $item1 = 239.99;
+    $item2 = 129.75;
+    $item3 = 99.95;
+    $item4 = 350.89;
+
+    $qnt_vendida_item1 = 3;
+    $qnt_vendida_item2 = 2;
+    $qnt_vendida_item3 = 5;
+    $qnt_vendida_item4 = 4;
+
+    $valor_vendas = ($qnt_vendida_item1*$item1)+($qnt_vendida_item2*$item2)+($qnt_vendida_item3*$item3)+($qnt_vendida_item4*$item4);
+    $comissao = number_format ($valor_vendas * 9/100,2);
+    $salario = $comissao +200;
+
+    echo "O vendedor vendeu: </br> 
+    $qnt_vendida_item1 do item 1 </br>
+    $qnt_vendida_item2 do item 2 </br>
+    $qnt_vendida_item3 do item 3 </br>
+    $qnt_vendida_item4 do item 4 </br>
+    </br> O valor da sua comissão é de: R$ $comissao </br>
+    Salário a receber: R$ $salario"
+    
+    ?>
+  </body>
+</html>

b76cdbb37bdb0cc6a0fbf3171f92386cbe2a1733
lucas <lucasvsteofilo@gmail.com>
2021-05-30 21:21:49 -0300

    exercicio08

---
+++ dojo/exercicio08.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Semana 01</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+
+    $numero1 = 30;
+    $numero2 = 20;
+
+    if ($numero1 < $numero2) {
+        echo "Entre os números $numero1 e $numero2 o menor é o $numero1";
+    } else {
+        echo "Entre os números $numero1 e $numero2 o menor é o $numero2";
+    }
+
+    ?>
+  </body>
+</html>

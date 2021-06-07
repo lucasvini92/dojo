e2195b1eaacef2917a89cc501d5155b4ccfdc869
Lucas Teofilo <lucas.teofilo@plataforma13.com.br>
2021-06-07 17:09:25 -0300

    exercicio02

---
+++ dojo-semana02/exercicio02.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Semana 02</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+
+    $num = 5;
+    
+    for ($i=1; $i <= 10 ; $i++) { 
+        $prod = $num * $i;
+        echo "$num x $i = $prod </br>";        
+    }
+
+    ?>
+  </body>
+</html>
